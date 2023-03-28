fb0b928846ee85d1447a3f1ecf73a0107efcf31f
Rukhillo Kodirov <rukhillo6663@gmail.com>
2023-03-28 17:30:27 +0900

    Unified two functions into one

--- script.js
+++ script.js
@@ -3,45 +3,36 @@ let add = document.querySelector("#add");
 let subract = document.querySelector("#subtract");
 let message = document.querySelector('h1')
 
-//Function for add btn
-function addition(){
-    let output = document.querySelector("#output");
-    output.innerHTML = Number(output.innerHTML) + 1;
-    result = output.innerHTML;
-      
-        if (result <0) 
-        {
-            output.style.backgroundColor = 'red'
-        } else if (result>0)
-        {
-            output.style.backgroundColor = 'navy'
-        } else  {
-            output.style.backgroundColor = 'grey';
 
-        } 
-      
-}
-//Function for subtract btn
-function subtration () {
+//Function for subtract and add btn
+function subtrationAndAdditon (event) {
     let output = document.querySelector("#output");
-    output.innerHTML = Number(output.innerHTML) - 1;
-    result = output.innerHTML;
+    let target = event.target.id;
+    //Check if it is subtract btn
+      if(target==='subtract')
+    {
+        output.innerHTML = Number(output.innerHTML) - 1;
+        result = output.innerHTML;
+    
+       // Check if it is btn
+    } else if (target==='add')
+    { 
+        output.innerHTML = Number(output.innerHTML) + 1;
+        result = output.innerHTML;
 
-  
-       if (result <0) 
-        {
+    }
+    if (result <0) 
+    {
           output.style.backgroundColor = 'red'
-        } else if (result>0)
-        {
-          output.style.backgroundColor = 'navy'
-        } else 
-        {
+    } else if (result>0)
+    {
+          output.style.backgroundColor = 'navy'    
+    } else 
+    {
           output.style.backgroundColor = 'grey';
-        }
-  
-   
+    } 
 }
 
 //AddEventListener trigger
-add.addEventListener("click", addition);
-subract.addEventListener("click", subtration)
+add.addEventListener("click", subtrationAndAdditon);
+subract.addEventListener("click", subtrationAndAdditon)
